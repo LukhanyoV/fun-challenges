8f0291ecfd921775b25b8d143b307e086cc73de5
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-06-13 10:42:42 +0200

    was failing for 3x-2x=4 formart

--- javascript/solve_for_x.js
+++ javascript/solve_for_x.js
@@ -2,21 +2,23 @@
 // given: 2x-4=0
 
 const solve_x = eq => {
-    const sides = eq.split("=")
+  const sides = eq.split("=")
 
-    const regex_int = /(\+|\-)?\d+(?![a-z])/g
-    
-    const regex_strn = /(\+|\-)?\d+(?=[a-z])/g
+  const regex_int = /(\+|\-)?\d+(?![a-z])/g
+  
+  const regex_strn = /(\+|\-)?\d+(?=[a-z])/g
 
-    const ints = sides.map(side => side.match(regex_int)).filter(s=>s).map(x => {
-      return x.map(Number).reduce((s,v)=>s+v,0)
-    }).map((v,i) => i === 0 ? -v : v).reduce((s,v)=>s+v)
-                                                                           
-    const strn = sides.map(side => side.match(regex_strn)).filter(s=>s).map(x => {
-      return x.map(Number).reduce((s,v)=>s+v,0)
-    }).map((v,i) => i !== 0 ? -v : v).reduce((s,v)=>s+v)
-    
-    return ints/strn
+  let ints = sides.map(side => side.match(regex_int)).filter(s=>s).map(x => {
+    return x.map(Number).reduce((s,v)=>s+v,0)
+  })
+  ints = ints.length > 1 ? ints.map((v,i) => i === 0 ? -v : v).reduce((s,v)=>s+v) : ints.reduce((s,v)=>s+v)
+                                                                         
+  let strn = sides.map(side => side.match(regex_strn)).filter(s=>s).map(x => {
+    return x.map(Number).reduce((s,v)=>s+v,0)
+  })
+  strn = strn.length > 1 ? strn.map((v,i) => i !== 0 ? -v : v).reduce((s,v)=>s+v) : strn.reduce((s,v)=>s+v)
+  
+  return ints/strn
 }
 
 // test the solution
